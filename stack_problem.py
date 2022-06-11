a225b49f62c95a1376661d76f1c76192d9107aae
Rahim Hossain <rahimhossain23@gmail.com>
2022-06-11 06:56:34 +0600

    Stack related problems are added

---
+++ stack_problem.py
@@ -0,0 +1,62 @@
+from collections import deque
+
+class Stack:
+    def __init__(self):
+        self.container = deque()
+    
+    def push(self,val):
+        self.container.append(val)
+        
+    def pop(self):
+        return self.container.pop()
+    
+    def peek(self):
+        return  self.container[-1]
+    
+    def is_empty(self):
+        return len(self.container)==0
+    
+    def size(self):
+        return len(self.container)
+
+def reverse_string(val):
+    stack = Stack()
+    for letter in val:
+        stack.push(letter)
+    rstr = ''
+    while stack.size()!=0:
+        
+        rstr += stack.pop()
+    return rstr
+
+def is_match(ch1, ch2):
+    match_dict = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+    return match_dict[ch1] == ch2
+
+
+def is_balanced(s):
+    stack = Stack()
+    for ch in s:
+        if ch=='(' or ch=='{' or ch == '[':
+            stack.push(ch)
+        if ch==')' or ch=='}' or ch == ']':
+            if stack.size()==0:
+                return False
+            if not is_match(ch,stack.pop()):
+                return False
+
+    return stack.size()==0
+
+
+if __name__ == '__main__':
+    print(reverse_string("We will conquere COVID-19"))
+    print(is_balanced("({a+b})"))
+    print(is_balanced("))((a+b}{"))
+    print(is_balanced("((a+b))"))
+    print(is_balanced("((a+g))"))
+    print(is_balanced("))"))
+    print(is_balanced("[a+b]*(x+2y)*{gg+kk}"))
