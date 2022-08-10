da5f417965c686833ae736a61e1de5b23f3ab1f7
Rahim Hossain <rahimhossain23@gmail.com>
2022-08-10 08:16:11 +0600

    searchByValueDuplicate function implemented

--- linkedList.cpp
+++ linkedList.cpp
@@ -14,6 +14,11 @@ public:
     }
 };
 
+struct Test
+{
+    int position[1000];
+};
+
 void display(Node* head)
 {
     while(head != NULL)
@@ -43,17 +48,43 @@ int searchByValueUnique(Node* head, int val)
 {
     int count =1;
 
-    while(head->value != val)
-    {
-        head = head->Next;
-        if(head == NULL)
+    if(head == NULL)
         {
             return -1;
-        }
+        }  
+
+    while(head->value != val)
+    {
+       if(head->Next == NULL)
+        {
+            return -1;
+        }  
+        head = head->Next;
         count++;
     }
 
     return count;
+}
+
+Test searchByValueDuplicate(Node* head, int val)
+{
+    int count = 1;
+    Test t;
+    int k=1;
+    while(head != NULL)
+    {
+        if(head->value == val)
+        {
+            //cout<<count<<" ";
+            t.position[k]=count;
+            k++;
+        }
+        head = head->Next;
+        count++;
+    }
+
+    t.position[0]=k;
+    return t;
 }
 
 void insertAtEnd(Node* &head,int val)
@@ -110,11 +141,17 @@ int main()
     insertAtEnd(head,5);
     insertAtHead(head,3);
     insertAtHead(head,10);
-    insertAtAnyPosition(head,5,6);
+    insertAtAnyPosition(head,5,3);
     int listLen = len(head);
     cout<<"The length of the list is "<<listLen<<endl;
     display(head);
     int position=searchByValueUnique(head,3);
     cout<<"the value is in "<<position<<" position"<<endl;
+    Test T = searchByValueDuplicate(head,3);
+    for (int i = 1; i < T.position[0]; i++)
+    {
+        cout<<T.position[i]<<" ";
+    }
+    cout<<endl;
     return 0;
 }
