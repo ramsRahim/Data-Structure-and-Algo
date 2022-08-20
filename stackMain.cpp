032d9a63b6e26ae42f08454d5a4203a9077ccdd3
Rahim Hossain <rahimhossain23@gmail.com>
2022-08-21 02:22:15 +0600

    generic stack header file

--- stackMain.cpp
+++ stackMain.cpp
@@ -3,18 +3,69 @@
 
 using namespace std;
 
+int globalID = 100;
+
+class person{
+    string name;
+    int id;
+    float salary;
+
+public:
+    person(){
+        name = "";
+        id = -1;
+        salary = -1.0;
+    }
+    void setName(string name){
+        this->name = name;
+    }
+
+    void setSalary(float salary){
+        this->salary = salary;
+    }
+
+    person(string name,float salary)
+    {
+        setName(name);
+        setSalary(salary);
+        id = globalID;
+        globalID++;
+    }
+
+    int getID(){
+        return id;
+    }
+
+    string getName(){
+        return name;
+    }
+
+    float getSalary()
+    {
+        return salary;
+    }
+
+    void print(){
+        cout<<name<<" | "<<id<<" | "<<salary<<endl;
+    }
+
+
+};
+
 int main()
 {
-    Stack <pair<int,char>> st;
-
-    st.push(make_pair(1,'a'));
-    st.push(make_pair(2,'b'));
-    st.push(make_pair(3,'c'));
+    Stack <person> st;
+    person a ("Rahim Hossain",654.6);
+    person b ("Madiha Hossain",754.6);
+    person c ("Karim Hossain",854.6);
+    st.push(a);
+    st.push(b);
+    st.push(c);
 
     while (!st.empty()){
-        pair <int,char> chk;
-        chk = st.pop();
-        cout<<chk.first<<" | "<<chk.second<<endl;
+        person print;
+        print = st.pop();
+        print.print();
     }
        
     return 0;
