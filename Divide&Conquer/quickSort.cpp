73d1f8316ea4ea2f248adaedc5902eb1f9878ff2
Rahim Hossain <rahimhossain23@gmail.com>
2023-03-04 05:28:49 -0500

    quicksort implemented

--- Divide&Conquer/quickSort.cpp
+++ Divide&Conquer/quickSort.cpp
@@ -2,29 +2,70 @@
 
 using namespace std;
 
-void partition(int S[], int low, int high, int pivotpoint ){
-    int i,j;
-    int pivotitem;
-
-    pivotitem = S[low];
-    j= low;
-
-    for (i=low+1;i<=high;i++){
-        if(S[i]<pivotitem){
-            j++;
-            int temp = S[i];
-            S[i] = S[j];
-            S[j] = temp;
+int partition(int arr[], int start, int end)
+{
+ 
+    int pivot = arr[start];
+ 
+    int count = 0;
+    for (int i = start + 1; i <= end; i++) {
+        if (arr[i] <= pivot)
+            count++;
+    }
+ 
+    // Giving pivot element its correct position
+    int pivotIndex = start + count;
+    swap(arr[pivotIndex], arr[start]);
+ 
+    // Sorting left and right parts of the pivot element
+    int i = start, j = end;
+ 
+    while (i < pivotIndex && j > pivotIndex) {
+ 
+        while (arr[i] <= pivot) {
+            i++;
+        }
+ 
+        while (arr[j] > pivot) {
+            j--;
+        }
+ 
+        if (i < pivotIndex && j > pivotIndex) {
+            swap(arr[i++], arr[j--]);
         }
     }
-    pivotpoint = j;
-    int temp = S[low];
-    S[low] = S[pivotpoint];
-    S[pivotpoint] = temp;
-
+ 
+    return pivotIndex;
 }
-
-int main(){
-
+ 
+void quickSort(int arr[], int start, int end)
+{
+ 
+    // base case
+    if (start >= end)
+        return;
+ 
+    // partitioning the array
+    int p = partition(arr, start, end);
+ 
+    // Sorting the left part
+    quickSort(arr, start, p - 1);
+ 
+    // Sorting the right part
+    quickSort(arr, p + 1, end);
+}
+ 
+int main()
+{
+ 
+    int arr[] = { 9, 3, 4, 2, 1, 8 };
+    int n = 6;
+ 
+    quickSort(arr, 0, n - 1);
+ 
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+ 
     return 0;
 }
